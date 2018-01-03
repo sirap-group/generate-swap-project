be3f8c6eb87b3742aa9af0bb7fa48ab4aff81bb8
Rémi Becheras <rbecheras@sirap.fr>
2018-01-04 00:49:43 +0100

    create task swap-project:package that generates swap-package:default

--- src/lib/generator.js
+++ src/lib/generator.js
@@ -1,31 +1,29 @@
 import isValid from 'is-valid-app'
-import extend from 'extend'
-import path from 'path'
+
 import Logger from './utils/Logger'
+import { task } from './utils/utils'
 
 import generateDefaults from 'generate-defaults'
 import generateDest from 'generate-dest'
+import generatePackage from './subgenerators/generate-swap-package/generator'
 
 const log = new Logger('generate-swap-project')
 
 export default function (app) {
   if (!isValid(app, 'generate-swap-project')) return
 
-  /**
-   * Listen for errors
-   */
   app.on('error', ::log.error)
 
   /**
    * Plugins
    */
-
   app.use(generateDefaults)
 
   /**
    * Micro generators (as plugins)
    */
   app.register('destination-directory', generateDest)
+  app.register('package', generatePackage)
 
   /**
    * Scaffold out a(n) swap-project project. Also aliased as the [default](#default) task.
@@ -42,6 +40,20 @@ export default function (app) {
   })
 
   /**
+   * Write a `package.json` file to the current working directory.
+   * Call the `package:default` task from the sub generator `generate-swap-package`.
+   *
+   * ```sh
+   * $ gen swap-project:package
+   * ```
+   * @name package
+   * @api public
+   */
+  app.task('package', function (cb) {
+    app.generate(['package:default'], cb)
+  })
+
+  /**
    * Write a `test-file.txt` file to the current working directory.
    *
    * ```sh
@@ -50,7 +62,6 @@ export default function (app) {
    * @name file
    * @api public
    */
-
   task(app, 'testfile', 'test-file.txt')
 
   /**
@@ -63,23 +74,5 @@ export default function (app) {
    * @name default
    * @api public
    */
-
   app.task('default', ['project'])
-}
-
-/**
- * Create a task with the given `name` and glob `pattern`
- */
-
-function task (app, name, pattern, dependencies) {
-  app.task(name, dependencies || [], cb => file(app, pattern))
-}
-
-function file (app, pattern) {
-  const opts = extend({}, app.base.options, app.options)
-  const srcBase = opts.srcBase || path.join(__dirname, '..', '..', 'templates')
-  return app.src(pattern, {cwd: srcBase})
-    .pipe(app.renderFile('*', app.base.cache.data))
-    .pipe(app.conflicts(app.cwd))
-    .pipe(app.dest(app.cwd))
 }
