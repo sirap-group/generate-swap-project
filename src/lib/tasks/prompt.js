7623194422a30785de313926c1369f19ced0eb56
Rémi Becheras <rbecheras@sirap.fr>
2018-01-05 23:44:50 +0100

    refactor 'prompt' task to use a clean promise chain

--- src/lib/tasks/prompt.js
+++ src/lib/tasks/prompt.js
@@ -12,9 +12,10 @@
  * @todo to use radio or other custom prompt:
  * @see https://github.com/enquirer/prompt-radio/issues/3
  */
-export default async app => {
-  return async done => {
+export default app => {
+  return done => {
     if (app.options.prompt === false) return done()
+
     app.base.data(app.cache.data)
     app.base.set('cache.prompted', true)
 
@@ -25,159 +26,183 @@ export default async app => {
       // console.log({response, key, question, answers})
     })
 
+    let name, defaultHost, owner, files
+
     app.question('name', {
       message: 'Project name ?',
       default: 'my-swap-app'
     })
-    const {name} = await askPromise(['name'])
-    app.base.data({name})
+    askPromise(['name'])
+    .then(answers => {
+      name = answers.name
+      app.base.data({name})
 
-    app.question('dest', {
-      message: 'Project directory ?',
-      default: name
+      app.question('dest', {
+        message: 'Project directory ?',
+        default: name
+      })
+      app.question('description', {
+        message: 'Description ?',
+        default: `${name} SWAP app`
+      })
+      app.choices('githosts', {
+        message: 'Git host platform ?',
+        choices: ['github.com', 'gitlab.sirap.fr', 'gitlab.com']
+      })
+      app.question('owner', {
+        message: `Git host username ?`
+      })
+
+      // const {dest, description, githosts, owner} = await askPromise(['dest', 'description', 'githosts', 'owner'])
+      return askPromise(['dest', 'description', 'githosts', 'owner'])
     })
-    app.question('description', {
-      message: 'Description ?',
-      default: `${name} SWAP app`
+    .then(answers => {
+      const {githosts} = answers
+      owner = answers.owner
+      defaultHost = (githosts.length > 1) ? 'github.com' : githosts[0]
+      app.base.data(answers)
+
+      app.question('namespace', {
+        message: 'Project namespace ?',
+        default: answers.owner
+      })
+
+      return askPromise(['namespace'])
     })
-    app.choices('githosts', {
-      message: 'Git host platform ?',
-      choices: ['github.com', 'gitlab.sirap.fr', 'gitlab.com']
+    .then(({namespace}) => {
+      app.base.data({namespace})
+
+      const defaultHomepage = defaultHost === 'github.com'
+        ? `https://github.com/${namespace}/${name}`
+        : `https://gitlab.sirap.fr/${namespace}/${name}`
+      app.question('homepage', {
+        message: 'Project homepage ?',
+        default: defaultHomepage
+      })
+
+      const defaultIssues = defaultHost === 'github.com'
+        ? `https://github.com/${namespace}/${name}/issues`
+        : `https://gitlab.sirap.fr/${namespace}/${name}/issues`
+      const defaultRepository = defaultHost === 'github.com'
+        ? `git@github.com:${namespace}/${name}.git`
+        : `git@gitlab.sirap.fr:${namespace}/${name}.git`
+
+      app.question('issues', {
+        message: 'Issues URL ?',
+        default: defaultIssues
+      })
+      app.question('repository', {
+        message: 'Repository URL ?',
+        default: defaultRepository
+      })
+      app.question('version', {
+        message: 'Version ?',
+        default: '0.1.0'
+      })
+      app.question('license', {
+        message: 'License ?',
+        default: defaultHost === 'github.com' ? 'MIT' : 'UNLICENSED'
+      })
+      app.question('author.name', {
+        message: 'Author name ?'
+      })
+      app.question('author.username', {
+        message: 'Author username ?',
+        default: owner
+      })
+
+      return askPromise([ 'homepage', 'issues', 'repository', 'version', 'license', 'author.name', 'author.username' ])
     })
-    app.question('owner', {
-      message: `Git host username ?`
+    .then(answers => {
+      app.base.data(answers)
+
+      const authorUsername = answers.author.username
+      const defaultAuthorUrl = defaultHost === 'github.com'
+        ? `https://github.com/${authorUsername}`
+        : `https://gitlab.sirap.fr/${authorUsername}`
+
+      app.question('author.url', {
+        message: 'Author URL ?',
+        default: defaultAuthorUrl
+      })
+      app.question('author.twitter', {
+        message: 'Twitter URL ?',
+        default: `https://twitter.com/${authorUsername}`
+      })
+
+      return askPromise(['author.url', 'author.twitter'])
     })
+    .then(answers => {
+      app.base.data(answers)
 
-    const {dest, description, githosts, owner} = await askPromise(['dest', 'description', 'githosts', 'owner'])
-    const defaultHost = (githosts.length > 1) ? 'github.com' : githosts[0]
+      app.question('main', {
+        message: 'Main file ?',
+        default: 'index.js'
+      })
 
-    app.base.data({dest, description, githosts, owner})
-
-    app.question('namespace', {
-      message: 'Project namespace ?',
-      default: owner
+      return askPromise(['main'])
     })
+    .then(({main}) => {
+      app.base.data({main})
 
-    const {namespace} = await askPromise(['namespace'])
-    app.base.data({namespace})
+      const defaultFiles = [
+        main,
+        'LICENSE',
+        'README.md',
+        'dist/',
+        'package.json',
+        'yarn.lock'
+      ]
+      app.choices('files', {
+        message: 'Packaged files ?',
+        choices: defaultFiles
+      })
 
-    const defaultHomepage = defaultHost === 'github.com'
-      ? `https://github.com/${namespace}/${name}`
-      : `https://gitlab.sirap.fr/${namespace}/${name}`
-    app.question('homepage', {
-      message: 'Project homepage ?',
-      default: defaultHomepage
+      return askPromise(['files'])
     })
+    .then(answers => {
+      files = answers.files
 
-    const defaultIssues = defaultHost === 'github.com'
-    ? `https://github.com/${namespace}/${name}/issues`
-    : `https://gitlab.sirap.fr/${namespace}/${name}/issues`
-    app.question('issues', {
-      message: 'Issues URL ?',
-      default: defaultIssues
+      app.question('additionnalFiles', {
+        message: 'Additionnal files (comma separated) ?'
+      })
+
+      return askPromise('additionnalFiles')
     })
+    .then(({additionnalFiles}) => {
+      if (additionnalFiles && additionnalFiles.length) {
+        additionnalFiles.split(',')
+        .map(s => s.trim())
+        .forEach(adFile => files.push(adFile))
+      }
 
-    const defaultRepository = defaultHost === 'github.com'
-    ? `git@github.com:${namespace}/${name}.git`
-    : `git@gitlab.sirap.fr:${namespace}/${name}.git`
-    app.question('repository', {
-      message: 'Repository URL ?',
-      default: defaultRepository
+      app.base.data({files})
+
+      const defaultKeywords = [
+        'SWAP',
+        'SWAP App'
+      ]
+      app.choices('keywords', {
+        message: 'Keywords ?',
+        choices: defaultKeywords
+      })
+      app.question('additionnalKeywords', {
+        message: 'Additionnal keywords (comma separated) ?'
+      })
+
+      return askPromise(['keywords', 'additionnalKeywords'])
     })
+    .then(({keywords, additionnalKeywords}) => {
+      if (additionnalKeywords && additionnalKeywords.length) {
+        additionnalKeywords.split(',')
+        .map(s => s.trim())
+        .forEach(adKeyword => keywords.push(adKeyword))
+      }
 
-    app.question('version', {
-      message: 'Version ?',
-      default: '0.1.0'
+      app.base.data({keywords})
     })
-    app.question('license', {
-      message: 'License ?',
-      default: defaultHost === 'github.com' ? 'MIT' : 'UNLICENSED'
-    })
-    app.question('author.name', {
-      message: 'Author name ?'
-    })
-    app.question('author.username', {
-      message: 'Author username ?',
-      default: owner
-    })
-
-    let answers = await askPromise([ 'homepage', 'issues', 'repository', 'version', 'license', 'author.name', 'author.username' ])
-    app.base.data(answers)
-
-    const authorUsername = answers.author.username
-    const defaultAuthorUrl = defaultHost === 'github.com'
-      ? `https://github.com/${authorUsername}`
-      : `https://gitlab.sirap.fr/${authorUsername}`
-    app.question('author.url', {
-      message: 'Author URL ?',
-      default: defaultAuthorUrl
-    })
-    app.question('author.twitter', {
-      message: 'Twitter URL ?',
-      default: `https://twitter.com/${authorUsername}`
-    })
-
-    answers = await askPromise(['author.url', 'author.twitter'])
-    app.base.data(answers)
-
-    app.question('main', {
-      message: 'Main file ?',
-      default: 'index.js'
-    })
-
-    const {main} = await askPromise(['main'])
-    app.base.data({main})
-
-    const defaultFiles = [
-      main,
-      'LICENSE',
-      'README.md',
-      'dist/',
-      'package.json',
-      'yarn.lock'
-    ]
-    app.choices('files', {
-      message: 'Packaged files ?',
-      choices: defaultFiles
-    })
-
-    const {files} = await askPromise(['files'])
-
-    app.question('additionnalFiles', {
-      message: 'Additionnal files (comma separated) ?'
-    })
-
-    const {additionnalFiles} = await askPromise('additionnalFiles')
-
-    if (additionnalFiles && additionnalFiles.length) {
-      additionnalFiles.split(',')
-      .map(s => s.trim())
-      .forEach(adFile => files.push(adFile))
-    }
-
-    app.base.data({files})
-
-    const defaultKeywords = [
-      'SWAP',
-      'SWAP App'
-    ]
-    app.choices('keywords', {
-      message: 'Keywords ?',
-      choices: defaultKeywords
-    })
-    app.question('additionnalKeywords', {
-      message: 'Additionnal keywords (comma separated) ?'
-    })
-
-    const {keywords, additionnalKeywords} = await askPromise(['keywords', 'additionnalKeywords'])
-
-    if (additionnalKeywords && additionnalKeywords.length) {
-      additionnalKeywords.split(',')
-      .map(s => s.trim())
-      .forEach(adKeyword => keywords.push(adKeyword))
-    }
-
-    app.base.data({keywords})
+    .then(() => done())
+    .catch(err => done(err))
   }
 
   async function askPromise (keys) {
