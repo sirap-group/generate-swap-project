2799f5a7239b32eb403f18eb9ed2eaefe618211d
Rémi Becheras <rbecheras@sirap.fr>
2018-01-05 09:03:16 +0100

    update tests to add missing fake prompt values

--- src/tests/test.js
+++ src/tests/test.js
@@ -131,17 +131,21 @@ describe('generate-swap-project', function () {
   function beforeEachTest (silent, cli) {
     app = generate({silent, cli})
     app.cwd = actual()
+
+    app.option('prompt', false)
+    app.option('check-directory', false)
     app.option('dest', actual())
 
     // see: https://github.com/jonschlinkert/ask-when
     app.option('askWhen', 'not-answered')
 
-    // set default data to use in templates. feel free to remove anything
-    // that isn't used (e.g. if "username" isn't defined in templates, just remove it)
     app.data(pkg)
     app.data('project', pkg)
     app.data('username', 'foo')
     app.data('owner', 'foo')
+    app.data('repository', 'http://githost/namespace/repo.git')
+    app.data('issues', 'http://githost/namespace/repo/issues')
+    app.data('version', 'v5.5.5')
   }
 })
 
