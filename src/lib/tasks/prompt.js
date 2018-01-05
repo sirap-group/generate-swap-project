0fd5b5ab452296a241d95f8ec9e0948266200f2f
Rémi Becheras <rbecheras@sirap.fr>
2018-01-05 09:00:22 +0100

    Create custom questions queue

--- src/lib/tasks/prompt.js
+++ src/lib/tasks/prompt.js
@@ -9,83 +9,168 @@
  * ```
  * @name project:prompt
  * @api public
+ * @todo to use radio or other custom prompt:
+ * @see https://github.com/enquirer/prompt-radio/issues/3
  */
 export default app => {
   return done => {
     if (app.options.prompt === false) return done()
     app.base.data(app.cache.data)
-
     app.base.set('cache.prompted', true)
-    app.question('homepage', 'Project homepage ?')
-    app.question('customfield', 'Champ Perso ?')
 
-    // common question names
-    var keys = mapValues([
-      'name',
-      'description',
-      'owner',
-      'homepage',
-      'license',
-      'author.name',
-      'author.username',
-      'author.url',
-      'customfield'
-    ], app)
+    // @todo skip if answer exist in cache
+    // const skipCondition = app.option('hints') === false || app.option('skip') === false
 
-    if (keys.skip.length) {
-      app.log()
-      app.log('', app.log.yellow(app.log.bold(app.log.underline('Heads up!'))))
-      app.log()
-      app.log(' The following data from user environment and/or package.json')
-      app.log(' will be used to render templates (if applicable), and prompts')
-      app.log(' for these values will be skipped:')
-      app.log()
-      app.log(formatFields(app, keys.skip))
-      app.log(` Run with ${app.log.cyan('--noskip')} to disable this feature.`)
-      app.log()
-      app.log(' ---')
-      app.log()
-    }
+    app.on('answer', function (response, key, question, answers) {
+      // console.log({response, key, question, answers})
+    })
 
-    app.ask(keys.ask, done)
+    app.question('name', {
+      message: 'Project name ?',
+      default: 'my-swap-app'
+    })
+    app.ask(['name'], (err, answers) => {
+      if (err) {
+        done()
+        return
+      }
+
+      const name = answers.name
+      app.base.data('name', name)
+      answers.dest = name
+
+      app.question('dest', {
+        message: 'Project directory ?',
+        default: name
+      })
+
+      app.question('description', {
+        message: 'Description ?',
+        default: `${name} SWAP app`
+      })
+
+      app.choices('githosts', {
+        message: 'Git host platform ?',
+        choices: ['github.com', 'gitlab.sirap.fr', 'gitlab.com']
+      })
+      app.question('owner', {
+        message: `Git host username ?`
+      })
+
+      app.ask(['dest', 'description', 'githosts', 'owner'], (err, answers) => {
+        if (err) {
+          done()
+          return
+        }
+
+        const {githosts, owner} = answers
+        // const {dest, description, githosts, owner} = answers
+        const defaultHost = (githosts.length > 1) ? 'github.com' : githosts[0]
+
+        app.base.data(answers)
+        app.base.data({ defaultHost })
+        // app.base.data({
+        //   dest,
+        //   description,
+        //   githosts,
+        //   owner,
+        //   defaultHost
+        // })
+
+        app.question('namespace', {
+          message: 'Project namespace ?',
+          default: owner
+        })
+        .ask(['namespace'], (err, answers) => {
+          if (err) {
+            done()
+            return
+          }
+
+          const {namespace} = answers
+          app.base.data({namespace})
+
+          const defaultHomepage = defaultHost === 'github.com'
+            ? `https://github.com/${namespace}/${name}`
+            : `https://gitlab.sirap.fr/${namespace}/${name}`
+          app.question('homepage', {
+            message: 'Project homepage ?',
+            default: defaultHomepage
+          })
+
+          const defaultIssues = defaultHost === 'github.com'
+          ? `https://github.com/${namespace}/${name}/issues`
+          : `https://gitlab.sirap.fr/${namespace}/${name}/issues`
+          app.question('issues', {
+            message: 'Issues URL ?',
+            default: defaultIssues
+          })
+
+          const defaultRepository = defaultHost === 'github.com'
+          ? `git@github.com:${namespace}/${name}.git`
+          : `git@gitlab.sirap.fr:${namespace}/${name}.git`
+          app.question('repository', {
+            message: 'Repository URL ?',
+            default: defaultRepository
+          })
+
+          app.question('version', {
+            message: 'Version ?',
+            default: 'v0.1.0'
+          })
+          app.question('license', {
+            message: 'License ?',
+            default: defaultHost === 'github.com' ? 'MIT' : 'UNLICENSED'
+          })
+
+          app.question('author.name', {
+            message: 'Author name ?'
+          })
+          app.question('author.username', {
+            message: 'Author username ?',
+            default: owner
+          })
+
+          app.ask([
+            'homepage',
+            'issues',
+            'repository',
+            'version',
+            'license',
+            'author.name',
+            'author.username'
+          ], (err, answers) => {
+            if (err) {
+              done(err)
+              return
+            }
+
+            app.base.data(answers)
+            const authorUsername = answers.author.username
+            const defaultAuthorUrl = defaultHost === 'github.com'
+              ? `https://github.com/${authorUsername}`
+              : `https://gitlab.sirap.fr/${authorUsername}`
+            app.question('author.url', {
+              message: 'Author URL ?',
+              default: defaultAuthorUrl
+            })
+            app.question('author.twitter', {
+              message: 'Twitter URL ?',
+              default: `https://twitter.com/${authorUsername}`
+            })
+
+            app.ask(['author.url', 'author.twitter'], (err, answers) => {
+              if (err) {
+                done(err)
+                return
+              }
+
+              app.base.data(answers)
+              done()
+            })
+          })
+        })
+      })
+    })
   }
-}
-
-/**
- * Map values and filter out keys for data that has already been defined,
- * to avoid asking unnecessary questions. This can be overridden
- * with `--noskip`
- * @todo enable correct `skipCondition`
- */
-function mapValues (keys, app) {
-  // Always SKIP because of bad yielding of some values (owner, homepage, username...)
-  const skipCondition = true
-  // const skipCondition = app.option('hints') === false || app.option('skip') === false
-  if (skipCondition) {
-    return {ask: keys, skip: []}
-  }
-
-  var res = {ask: [], skip: []}
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i]
-    if (!app.has('cache.data', key)) {
-      res.ask.push(key)
-    } else {
-      app.base.data(key, app.data(key))
-      res.skip.push([key, app.data(key)])
-    }
-  }
-  return res
-}
-
-/**
- * Format skipped fields
- */
-function formatFields (app, keys) {
-  var list = ''
-  keys.forEach(key => {
-    list += '  · ' + app.log.bold(key[0])
-    list += ': ' + app.log.green(key[1]) + '\n'
-  })
-  return list
 }
