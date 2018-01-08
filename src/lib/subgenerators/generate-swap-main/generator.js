a07878fd98a79737056c9fa0485677748927bcf5
Rémi Becheras <rbecheras@sirap.fr>
2018-01-08 09:32:14 +0100

    create main, index-root, index-src and index-test tasks in swap-main sub-generator

--- src/lib/subgenerators/generate-swap-main/generator.js
+++ src/lib/subgenerators/generate-swap-main/generator.js
@@ -5,41 +5,77 @@ import Logger from '../../utils/Logger'
 
 import generateDefaults from 'generate-defaults'
 
-const log = new Logger('generate-swap-contribute')
+const log = new Logger('generate-swap-main')
 
 export default function (app) {
-  if (!isValid(app, 'generate-swap-contribute')) return
+  if (!isValid(app, 'generate-swap-main')) return
 
   app.on('error', ::log.error)
 
   app.use(generateDefaults)
 
   /**
-   * Write a `.contribute` file to the current working directory.
+   * Write the following three files to the current working directory:
+   * - ./index.js
+   * - ./src/lib/index.js
+   * - ./src/tests/index.test.js
    *
    * ```sh
-   * $ gen swap-contribute:contribute
+   * $ gen swap-main:main
    * ```
-   * @name contribute
+   * @name main
    * @api public
    */
-  task(app, 'contribute', 'generate-swap-contribute/contribute.md')
+  app.task('main', ['index-root', 'index-src', 'index-test'])
+
+  /**
+   * Create the root index file
+   *
+   * ```sh
+   * $ gen swap-main:index-root
+   * ```
+   * @name index-root
+   * @api public
+   */
+  task(app, 'index-root', 'generate-swap-main/index-root.js')
+
+  /**
+   * Create the src index file
+   *
+   * ```sh
+   * $ gen swap-main:index-src
+   * ```
+   * @name index-src
+   * @api public
+   */
+  task(app, 'index-src', 'generate-swap-main/index-src.js')
+
+  /**
+   * Create the test index file
+   *
+   * ```sh
+   * $ gen swap-main:index-test
+   * ```
+   * @name index-test
+   * @api public
+   */
+  task(app, 'index-test', 'generate-swap-main/index-test.js')
 
   /**
    * Run the `default` task
    *
    * ```sh
-   * $ gen swap-contribute
+   * $ gen swap-main
    * ```
    *
    * or
    *
    * ```sh
-   * $ gen swap-contribute:default
+   * $ gen swap-main:default
    * ```
    *
    * @name default
    * @api public
    */
-  app.task('default', ['contribute'])
+  app.task('default', ['main'])
 }
