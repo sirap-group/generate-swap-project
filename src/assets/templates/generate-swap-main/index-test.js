0bc49f4a4043e3d00bd878f2ddfbb701c6e7ea33
Rémi Becheras <rbecheras@sirap.fr>
2018-01-08 10:49:06 +0100

    fix issues in index-test.js template

--- src/assets/templates/generate-swap-main/index-test.js
+++ src/assets/templates/generate-swap-main/index-test.js
@@ -4,12 +4,11 @@ rename:
   basename: 'src/tests/index.test.js'
 ---
 import 'mocha'
-import path from 'path'
+// import path from 'path'
 import chai from 'chai'
 import debug from 'debug'
 
 import DemoClass from '../lib/'
-
 
 const expect = chai.expect
 
@@ -23,17 +22,16 @@ d('starting')
 
 let demoObj
 
-
 /**
  * @test <%= name %>
  */
-describe('<%= name %>', () => {
+describe('<%= name %>', function () {
   this.slow(250)
 
   if (isCI || isTravis) {
     before(done => {
-      const contextLabel = isTravis? 'travis' : 'CI'
-      d(`${contextLabel} test running`)
+      const contextLabel = isTravis ? 'travis' : 'CI'
+      d(contextLabel + `test running`)
       done()
     })
   }
@@ -52,33 +50,38 @@ describe('<%= name %>', () => {
    * @test DemoClass
    */
   describe('DemoClass', () => {
-    it('should be a valid class', () => {
+    it('should be a valid class', done => {
       expect(DemoClass).to.be.a('function')
       expect(DemoClass).to.have.a.property('prototype')
       expect(DemoClass.prototype).to.be.an('object')
+      done()
     })
     it('should have a `concatenate` method', done => {
       expect(DemoClass.prototype).to.have.a.property('concatenate')
       expect(DemoClass.prototype.concatenate).to.be.a('function')
+      done()
     })
 
     /**
      * @test instance
      */
     describe('instance', () => {
-      it('should be an instance of DemoClass', () => {
+      it('should be an instance of DemoClass', done => {
         expect(demoObj).to.be.an('object')
         expect(demoObj).to.be.an.instanceOf(DemoClass)
+        done()
       })
       it('should have a `propA` string property equal to A', done => {
         expect(demoObj).to.have.a.property('propA')
         expect(demoObj.propA).to.be.a('string')
         expect(demoObj.propA).to.equal('A')
+        done()
       })
       it('should have a `propB` string property equal to B', done => {
         expect(demoObj).to.have.a.property('propB')
         expect(demoObj.propB).to.be.a('string')
         expect(demoObj.propB).to.equal('B')
+        done()
       })
     })
 
@@ -86,10 +89,11 @@ describe('<%= name %>', () => {
      * @test #concatenate()
      */
     describe('#concatenante', () => {
-      it('should concatenante demoObj.propA and demoObj.propB', () => {
+      it('should concatenante demoObj.propA and demoObj.propB', done => {
         let ret = demoObj.concatenate()
         expect(ret).to.be.a('string')
         expect(ret).to.equal('AB')
+        done()
       })
     })
   })
