5b344104c93199b5a9958dac7158d0be747cafad
Rémi Becheras <rbecheras@sirap.fr>
2018-01-08 11:13:37 +0100

    create camelcase template helper

--- src/lib/utils/helpers.js
+++ src/lib/utils/helpers.js
@@ -1,7 +1,13 @@
 'use strict'
 
-export { escapeQuotes }
+export { escapeQuotes, camelcase }
 
 function escapeQuotes (str) {
   return str.replace(/\\?"/g, '\\"')
+}
+
+function camelcase (str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
+    return index === 0 ? letter.toLowerCase() : letter.toUpperCase()
+  }).replace(/\s+/g, '')
 }
