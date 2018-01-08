f034dd3409b5d8475334fb7031b52571fcd370ab
Rémi Becheras <rbecheras@sirap.fr>
2018-01-08 11:33:48 +0100

    fix task swap-readme:readme,  create task swap-readme:assets and add task assets to swap-readme:default

--- src/lib/subgenerators/generate-swap-readme/generator.js
+++ src/lib/subgenerators/generate-swap-readme/generator.js
@@ -1,5 +1,8 @@
+import path from 'path'
+import extend from 'extend'
 import isValid from 'is-valid-app'
 import helperDate from 'helper-date'
+// import copy from 'copy'
 
 import { task } from '../../utils/utils'
 import { camelcaseHelper } from '../../utils/helpers'
@@ -17,61 +20,54 @@ export default function (app) {
   app.helper('date', helperDate)
   app.helper('camelcase', camelcaseHelper)
 
-  app.postRender(/readme\.json$/, packagePostRender(app))
-
   app.use(generateDefaults)
 
   /**
-   * Write a `readme.json` file to the current working directory.
+   * Write a `README.md` file to the current working directory.
    *
    * ```sh
    * $ gen swap-readme:readme
    * ```
-   * @name file
+   * @name readme
    * @api public
    */
-  task(app, 'readme', 'generate-swap-readme/$readme.json')
+  task(app, 'readme', 'generate-swap-readme/readme.md')
 
   /**
-   * Run the `readme` task
+   * Write required assets images to src/assets/img
    *
    * ```sh
-   * $ gen swap-project
+   * $ gen swap-readme:readme
+   * ```
+   * @name readme
+   * @api public
+   */
+  app.task('assets', done => {
+    const opts = extend({}, app.base.options, app.options)
+    const srcBase = opts.srcBase || path.join(__dirname, '../../../assets')
+    const dest = path.join(app.cwd, 'src/assets/img')
+
+    console.log({ dest, srcBase })
+    // copy('../../../assets/img/*', dest, done)
+    return app.src('img/*', {cwd: srcBase})
+    .pipe(app.dest(dest))
+  })
+
+  /**
+   * Run the `default` task (assets, readme)
+   *
+   * ```sh
+   * $ gen swap-readme
    * ```
    *
    * or
    *
    * ```sh
-   * $ gen swap-project:default
+   * $ gen swap-readme:default
    * ```
    *
-   * @name file
+   * @name default
    * @api public
    */
-  app.task('default', ['readme'])
-}
-
-function packagePostRender (app) {
-  return (file, next) => {
-    if (!file.basename === 'readme.json') {
-      next()
-      return
-    }
-
-    // Load the readme a an object
-    const pkg = JSON.parse(file.content)
-
-    // Set the readme private if --private
-    if (app.options.private) {
-      pkg.private = true
-    }
-
-    // Format readme.json#files as an array
-    pkg.files = pkg.files.split(',')
-    // Format readme.json#keywords as an array
-    pkg.keywords = pkg.keywords.split(',')
-
-    file.contents = Buffer.from(JSON.stringify(pkg, null, 2))
-    next()
-  }
+  app.task('default', ['assets', 'readme'])
 }
